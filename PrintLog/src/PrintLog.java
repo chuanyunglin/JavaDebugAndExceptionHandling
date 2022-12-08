d8734a588624a4e8dfaa9faef06f030498185e76
ChuanyungLin <chuanyung.lin@tpisoftware.com>
2022-12-08 17:16:43 +0800

    測試 System.out 和 System.err 的差別

---
+++ PrintLog/src/PrintLog.java
@@ -0,0 +1,12 @@
+
+public class PrintLog {
+
+	public static void main(String[] args) {
+		System.out.println("1");
+		System.err.println("2");
+		System.out.println("3");
+		System.err.println("4");
+		System.out.println("5");
+		System.out.println("6");
+	}
+}
