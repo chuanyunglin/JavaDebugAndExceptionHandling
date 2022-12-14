76f2d2a1856a8124bb63a29b3f23af1d50817bcd
ChuanyungLin <chuanyung.lin@tpisoftware.com>
2022-12-14 10:38:01 +0800

    添加 Deadlock 範例

---
+++ Deadlock/src/Loop.java
@@ -0,0 +1,12 @@
+
+public class Loop {
+
+	public static void main(String[] args) {
+		System.out.println("Start loop");
+		boolean stop = false;
+		while (!stop) {
+		}
+		System.out.println("End loop");
+	}
+
+}
