0d80f23e3ec07f97b70dd6d726406dbf3e9bbc38
ChuanyungLin <chuanyung.lin@tpisoftware.com>
2022-12-15 14:53:08 +0800

    添加 TriangleTest 專案，使用 debugger 修正問題。

---
+++ TriangleTest/src/TriangleTest.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+public class TriangleTest {
+	
+	private static class Triangle {
+		int sideA;
+		int sideB;
+		int sideC;
+		
+		public Triangle(int a, int b, int c) {
+			sideA = a;
+			sideB = b;
+			sideC = b;
+		}
+		
+		public boolean isLegal() {
+			if ((sideA + sideB > sideC) || (sideA + sideC > sideB)) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+		
+		public boolean isRightAngle() {
+			int max = Math.max(sideA, Math.max(sideB, sideC));
+			int min = Math.min(sideA, Math.max(sideB, sideC));
+			int median = sideA + sideB + sideC - max - min;
+			return max * max == min * min + median + median;
+		}
+		
+		public void show() {
+			System.out.printf("(%d, %d, %d) is " + (isLegal() ? "legal\t" : "not legal\t"), sideA, sideB, sideC);
+			System.out.println(isLegal() ? "right" : "not right");
+		}
+	}
+	
+	public static void main(String[] args) {
+		ArrayList<Triangle> list = new ArrayList<Triangle>();
+		list.add(new Triangle(6, 4, -1));
+		list.add(new Triangle(1, 2, 3));
+		list.add(new Triangle(4, 5, 6));
+		list.add(new Triangle(3, 5, 4));
+		list.add(new Triangle(12, 13, 5));
+		list.add(new Triangle(7, 3, 8));
+
+		for (Triangle tr : list) {
+			tr.show();
+		}
+	}
+}
